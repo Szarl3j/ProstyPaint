f3d824c34f4328fc60f62c57dbc8d8fea60b1fbf
Tomasz Moch <52713576+Szarl3j@users.noreply.github.com>
2026-03-25 11:48:44 +0100

    Add files via upload
    Naprawa błędu. Uzupełnienie brakujących plików

---
+++ properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PaintApplicationAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PaintApplicationAssignment")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ED7F6E6-E874-4639-8C14-272BA98F53DF")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
